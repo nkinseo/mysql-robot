50531e3dfd860b5773e6b441fbddae4b917efe6b
minseo <kmsdkyb@gmail.com>
2025-06-05 14:22:56 +0900

    🎨 테이블 접근

---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20200014/mysql_robot/RobotService.kt
@@ -0,0 +1,12 @@
+package kr.ac.kumoh.ce.s20200014.mysql_robot
+
+import org.springframework.stereotype.Service
+
+@Service
+class RobotService(val repository: RobotRepository) {
+//    fun getAllRobots(): List<Robot> {
+//        return repository.findAll()
+//    }
+
+    fun getAllRobots(): List<Robot> = repository.findAll()
+}
